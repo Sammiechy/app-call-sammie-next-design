2c73deef985ba129a36e0e1b5c0dc1e3e1f2ee03
shammi <amrita.choudhary41@gmail.com>
2024-09-22 01:50:23 +0530

    template design

---
+++ components/Header.tsx
@@ -0,0 +1,51 @@
+// components/Header.js
+import React from 'react';
+import Link from 'next/link';
+import styles from './header.module.css'; 
+import Image from "next/image";
+
+const Header = () => {
+  return (
+    <header className={styles.header}>
+
+      <div className={styles.container}>
+        {/* Logo Section */}
+        <div className={styles.logo}>
+        <Image
+  src="/assets/Vector.png" 
+  alt="Logo" 
+  width={40} 
+  height={40} 
+  className={styles.logoImage} 
+/>
+          <span className={styles.brandName}>uifry</span>
+        </div>
+
+        {/* Navigation Links */}
+        <nav className={styles.nav}>
+          <ul className={styles.navList}>
+            <li className={styles.navItem}>
+              <Link href="/">Home</Link>
+            </li>
+            <li className={styles.navItem}>
+              <Link href="/about">About Us</Link>
+            </li>
+            <li className={styles.navItem}>
+              <Link href="/pricing">Pricing</Link>
+            </li>
+            <li className={styles.navItem}>
+              <Link href="/features">Features</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* Download Button */}
+        <div className={styles.downloadButtonContainer}>
+          <button className={styles.downloadButton}>Download</button>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
